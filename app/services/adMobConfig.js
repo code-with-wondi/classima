b7591451e369c98ffe99b9fca2c38ad549713ff8
code-with-wondi <wendimar778@gmail.com>
2023-07-13 10:16:32 +0300

    first commit

---
+++ app/services/adMobConfig.js
@@ -0,0 +1,9 @@
+export const admobConfig = {
+  admobEnabled: false,
+  admobBannerId: {
+    android: "ca-app-pub-45656767879/63009789908111",
+    iOS: "ca-app-pub-5676879789/4564556",
+  },
+  listAdType: "largeBanner",
+  detailScreenAdType: "largeBanner",
+};
---
+++ app/services/socialLoginConfig.js
@@ -0,0 +1,7 @@
+export const socialConfig = {
+  enabled: false,
+  socialPlatforms: [],
+  google: {
+    webClientId: "876765765-j87ht987yh876h86n97gt6.apps.googleusercontent.com",
+  },
+};
